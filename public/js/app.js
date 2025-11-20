fb6ec7bf97be3537020d1e05a259e30005363943
Faldu Harin <160569922+harinthegreat@users.noreply.github.com>
2025-11-20 23:21:33 +0530

    Update battle code generation

--- public/js/app.js
+++ public/js/app.js
@@ -93,7 +93,7 @@ class BattleshipGame {
 
     generateBattleCode() {
         // Generate a 6-character alphanumeric code
-        const chars = 'ABCDEF0123456789';
+        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
         let code = '';
         for (let i = 0; i < 6; i++) {
             code += chars.charAt(Math.floor(Math.random() * chars.length));
